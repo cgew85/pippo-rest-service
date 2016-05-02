11da06da5e660d7855180dbf3a8ebcafa8312334
cgew85 <christoph.gewehr@mailbox.org>
2016-05-02 19:14:49 +0200

    Initial commit #2

---
+++ src/main/java/io/github/cgew85/PippoRestServiceApplication.java
@@ -0,0 +1,12 @@
+package io.github.cgew85;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PippoRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PippoRestServiceApplication.class, args);
+	}
+}
